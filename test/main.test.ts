5f509094dfd600daf97657989a879df52b160b09
kotekpsotek <siematumichael@protonmail.com>
2024-10-08 23:33:07 +0200

    Last modificationns

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,8 +1,8 @@
 import { randomUUID } from "node:crypto";
 import { it, describe, expect } from "vitest";
 import { checkHourFormat, checkHoursRange } from "../src/lib"
-import { Wagons, WagonsSet } from "../src/lib/wagons"
-import { Personel } from "../src/lib/personel"
+// import { Wagons, WagonsSet } from "../src/lib/wagons"
+// import { Personel } from "../src/lib/personel"
 import { DrivePlan, WagonsData } from "../src/lib/drivetime";
 
 describe("Test dependecies", () => {
@@ -59,77 +59,6 @@ describe("Test dependecies", () => {
     });
 })
 
-// TODO: Improve granuality of tests
-describe("Suggestions", () => {
-    it("Wagons suggestion: Number to handle clients efficiently", () => {
-        // Small Number
-        const w2 = new Wagons(55);
-        const c2 = (w2.payloadCalculateWagons())[1] as WagonsSet;
-        
-        expect(c2).toBeTypeOf("object");
-        expect(c2.length).toBe(1);
-        expect(c2[0].wagonType).toBe(75);
-        expect(c2[0].wagonsCount).toBe(1);
-        
-        // Basic Number
-        const w1 = new Wagons(175);
-        const c1 = w1.payloadCalculateWagons()[1] as WagonsSet;
-
-        expect(c1).toBeTypeOf("object");
-        expect(c1[0].wagonType).toBe(75);
-        expect(c1[0].wagonsCount).toBe(2);
-
-        expect(c1).toBeTypeOf("object");
-        expect(c1[1].wagonType).toBe(32);
-        expect(c1[1].wagonsCount).toBe(1);
-
-        // Format wagons
-            // PL
-        const f1 = w1.format("PL");
-        expect(f1.length).toBeGreaterThan(5);
-        console.log(f1)
-            // EN
-        const f2 = w1.format("EN");
-        expect(f2.length).toBeGreaterThan(5);
-        console.log(f2)
-    })
-
-    it("Personel suggestions", () => {
-        // Initialize what is required
-        const wagons = new Wagons(20000);
-        const personel = new Personel(wagons.payloadCalculateWagons()[1] as WagonsSet);
-        
-        // Wagons
-        const personelWagons = personel
-            .wagonsCalculatePersonel()
-            .personelPerWagons;
-        expect(personelWagons).toBeTypeOf("object");
-        // expect(personel.wagonsTypesSet.length).toBe(1) // FIXME: uncomment
-        expect(personel.wagonsTypesSet[0].wagonType).toBe(75 as WagonSeats)
-
-
-            // Polish language test
-        const formattedWagonsPL = personel.wagonsFormat("PL");
-        expect(formattedWagonsPL).toBeTypeOf("string");
-        expect(formattedWagonsPL.length).toBeGreaterThanOrEqual(10)
-
-            // English language test
-        const formattedWagonsEN = personel.wagonsFormat("EN");
-        expect(formattedWagonsEN).toBeTypeOf("string");
-        expect(formattedWagonsEN.length).toBeGreaterThanOrEqual(10)
-        console.log(formattedWagonsEN)
-
-        // Coaster itself
-            // English
-        const boardingCoasterTeamEN = personel.boardingCoasterFormat("EN");
-        expect(boardingCoasterTeamEN.length).toBeGreaterThanOrEqual(5);
-            // Polish
-        const boardingCoasterTeamPL = personel.boardingCoasterFormat("PL");
-        expect(boardingCoasterTeamPL.length).toBeGreaterThanOrEqual(5);
-        console.log(boardingCoasterTeamEN)
-    })
-})
-
 describe("Drive Plan", () => {
     const coaster = {
         clients_count: 70,
@@ -144,21 +73,21 @@ describe("Drive Plan", () => {
                 seats: 32,
                 speed_m_per_s: 5.6
             },
-            // {
-            //     id: randomUUID(),
-            //     seats: 32,
-            //     speed_m_per_s: 8000
-            // },
-            // {
-            //     id: randomUUID(),
-            //     seats: 32,
-            //     speed_m_per_s: 15.5
-            // }, 
-            // {
-            //     id: randomUUID(),
-            //     seats: 75,
-            //     speed_m_per_s: 15.5
-            // }
+            {
+                id: randomUUID(),
+                seats: 32,
+                speed_m_per_s: 8000
+            },
+            {
+                id: randomUUID(),
+                seats: 32,
+                speed_m_per_s: 15.5
+            }, 
+            {
+                id: randomUUID(),
+                seats: 75,
+                speed_m_per_s: 15.5
+            }
         ]
         const drivePlanIns = new DrivePlan(coaster, wagons);
         const c = drivePlanIns.computeDrivePlan();
